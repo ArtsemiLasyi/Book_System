30593c313ea23ba0b31fc5f05c685501b48cc751
ArtsemiLasyi <50248368+ArtsemiLasyi@users.noreply.github.com>
2020-09-06 17:27:40 +0300

    Добавьте файлы проекта.

---
+++ Book_System/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_System
+{
+    public class Book
+    {
+        public String Name { get; private set; }
+        public String Author { get; private set; }
+        public decimal Value { get; private set; }
+
+        public Book()
+        {
+            this.Name = "";
+            this.Author = "";
+            this.Value = 0.00m;
+        }
+    }
+}
